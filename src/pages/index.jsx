585dddd3bbebb803954f0f0e22a857536ade1ac1
Winaya Zarkasih <winaya@sawala.tech>
2023-09-06 13:38:48 +0700

    feat(component): define navbar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,40 +1,18 @@
-import { useState } from "react";
-import { ModalComponent } from "../components/Modal";
-import { Button, Input, Textarea, Select, Stack, HStack, Text } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react"
+import { BoardCard } from "../components/Cards"
+import { Navbar } from "../components/Navbar"
 
 const LandingPage = () => {
-    const [openModal, setOpenModal] = useState(false);
-
-    const handleOpenModal = () => {
-        setOpenModal(true);
-    };
-
-    return (
-        <>
-            <Button onClick={handleOpenModal}>Tambah Tugas</Button>
-            <ModalComponent title="Tugas baru" openModal={openModal} onClose={() => setOpenModal(false)}>
-                <Stack spacing={3}>
-                    <HStack spacing='24px'>
-                        <Text w={'35%'} fontWeight={600}>Nama Tugas</Text>
-                        <Input placeholder='Tugas 1 : Belajar HTML' />
-                    </HStack>
-                    <HStack spacing='24px'>
-                        <Text w={'35%'} fontWeight={600}>Deskripsi</Text>
-                        <Textarea placeholder='Buat contoh struktur HTML sederhana untuk pembuatan aplikasi web' />
-                    </HStack>
-                    <HStack spacing='24px'>
-                        <Text w={'35%'} fontWeight={600}>Status</Text>
-                        <Select>
-                            <option value='option1'>Open</option>
-                            <option value='option2'>In progress</option>
-                            <option value='option3'>Done</option>
-                        </Select>
-                    </HStack>
-                </Stack>
-                <Button colorScheme='blue' mt={4}>Simpan</Button>
-            </ModalComponent>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Grid templateColumns="repeat(3, 1fr)">
+        <BoardCard headerText="Open" headingColor="#38B2AC" boardBg="#ebf8ff" />
+        <BoardCard headerText="In Progress" headingColor="#4299E1" />
+        <BoardCard headerText="Done" headingColor="#9F7AEA" BoardBg="#ebf8ff" />
+      </Grid>
+    </>
+  )
 }
 
-export default LandingPage;
+export default LandingPage
